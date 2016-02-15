da0aebf3055159f1a25c236df5b0e35ff7b89931
RezaSR <rsr_reza@yahoo.com>
2016-02-15 20:09:14 +0330

    Undo stash of get vendor info

--- dbf/server.go
+++ dbf/server.go
@@ -30,51 +30,6 @@ func setDefaultHeader(req *http.Request) {
 	req.Header.Set("Content-Type", "application/json")
 }
 
-func getVendorInfo(vendorType *string, vendorName *string, platformId *string, vendorInfoPath *string) bool {
-	reqJson := `{"vendor_type":"` + *vendorType + `","name":"` + *vendorName + `","platform_id":"` + *platformId + `"}`
-
-	req, err := http.NewRequest("POST", serverUrl+_URL_GET_VENDOR_INFO, bytes.NewBufferString(reqJson))
-	if err != nil {
-		Log.Printf("%s\n", err)
-		return false
-	}
-
-	setDefaultHeader(req)
-	req.Header.Set("Accept", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		Log.Printf("%s\n", err)
-		return false
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		Log.Printf("Bad response from server:\nStatus: %s\nHeaders: %s\n", resp.Status, resp.Header)
-		return false
-	}
-
-	// Process response
-	vendorInfoFile, err := os.OpenFile(*vendorInfoPath+".tmp", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
-	if err != nil {
-		Log.Printf("%s\n", err)
-		vendorInfoFile.Close()
-		return false
-	}
-
-	_, err = io.Copy(vendorInfoFile, resp.Body)
-	if err != nil {
-		Log.Printf("%s\n", err)
-		return false
-	}
-
-	vendorInfoFile.Close()
-
-	os.Rename(*vendorInfoPath+".tmp", *vendorInfoPath)
-
-	return true
-}
-
 func getVendor(vendorType *string, vendorName *string, platformId *string, vendorPath *string) bool {
 	reqJson := `{"vendor_type":"` + *vendorType + `","name":"` + *vendorName + `","platform_id":"` + *platformId + `"}`
 
