e05d9f53ae0815f753e1d0df5fc1b133177c7ddf
RezaSR <rsr_reza@yahoo.com>
2016-02-28 09:57:18 +0330

    No need to get vendor info

--- dbf/server_const.go
+++ dbf/server_const.go
@@ -1,12 +1,10 @@
 package dbf
 
 const (
-	_URL_GET_VENDOR_INFO    = "vendor/getinfo"
-	_URL_GET_VENDOR         = "vendor/get"
-	_URL_UPDATE_VENDOR_INFO = "vendor/updateinfo"
-	_URL_UPDATE_VENDOR      = "vendor/update"
-	_URL_GET_TASK           = "task/get"
-	_URL_GET_CRACK_INFO     = "crack/info"
-	_URL_GET_CRACK_DEP      = "crack/dep"
-	_URL_SEND_RESULT        = "task/result"
+	_URL_GET_VENDOR     = "vendor/get"
+	_URL_UPDATE_VENDOR  = "vendor/update"
+	_URL_GET_TASK       = "task/get"
+	_URL_GET_CRACK_INFO = "crack/info"
+	_URL_GET_CRACK_DEP  = "crack/dep"
+	_URL_SEND_RESULT    = "task/result"
 )
