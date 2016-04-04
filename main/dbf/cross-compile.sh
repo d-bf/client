71c80fd738577dd9b1f23b419466b82fccdc2bb6
RezaSR <rsr_reza@yahoo.com>
2016-04-04 21:29:48 +0330

    Release in the same dir and as dbf name

--- main/dbf/cross-compile.sh
+++ main/dbf/cross-compile.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 
-# Set the GOPATH if it is not set or if is different when running as another user (sudo)
-# export GOPATH="" 
-
 PATH_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PATH_BUILD="$GOPATH/bin/dbf_release"
+PATH_BUILD="$PATH_SCRIPT/dbf_release"
 mkdir -p $PATH_BUILD
 
 # Linux
@@ -13,30 +10,30 @@ export GOOS="linux"
 
 export GOARCH="amd64"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/linux_64"
+go build -o "$PATH_BUILD/linux_64/dbf"
 
 export GOARCH="386"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/linux_32"
+go build -o "$PATH_BUILD/linux_32/dbf"
 
 # Windows
 export GOOS="windows"
 
 export GOARCH="amd64"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/win_64"
+go build -o "$PATH_BUILD/win_64/dbf.exe"
 
 export GOARCH="386"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/win_32"
+go build -o "$PATH_BUILD/win_32/dbf.exe"
 
 # Darwin
 export GOOS="darwin"
 
 export GOARCH="amd64"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/mac_64"
+go build -o "$PATH_BUILD/mac_64/dbf"
 
 export GOARCH="386"
 echo "Compiling... (OS: $GOOS, ARCH: $GOARCH)"
-go build -o "$PATH_BUILD/mac_32"
+go build -o "$PATH_BUILD/mac_32/dbf"
